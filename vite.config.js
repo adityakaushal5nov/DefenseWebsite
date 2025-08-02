7bca02e39e42b7be2d902c2ec54a52265be4d36e
Aditya Kaushal <kaushal.aditya5nov@gmail.com>
2025-08-02 19:24:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DefenseWebsite/', // Updated to match your repo name
+  base: '/DefenseWebsite/', // Update to match your repo name
 })
